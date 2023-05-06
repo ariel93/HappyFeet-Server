211600896c3543bca3a31af6b7f9db22b204ef7d
SergeyAro <122204687+SergeyAro@users.noreply.github.com>
2023-05-06 20:51:41 +0300

    Setting up a server with Node.js, Express.js, Mongoose, and TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose';
 import ShoeRouter from './shoe/routes/shoe.router';
 
 const port = 3000;
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 const dbUser: String = 'ariel93yohimovich';
 const dbPassword = 'HJHDdtrh10B6Bx5Y';
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.oi2q24o.mongodb.net/?retryWrites=true&w=majority`, { dbName: "HappyFeet" })
-  .then(() => console.log('Connected to MongoDB'));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 
 app.use('/shoes', ShoeRouter);
